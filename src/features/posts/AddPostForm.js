170c5001394127c0dcbf73fb2f9e767680b91e96
Ailee-qiu <2984413538@qq.com>
2023-07-25 23:56:45 +0800

    0725_night

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,41 +1,65 @@
-import { nanoid } from '@reduxjs/toolkit'
-import { Button, Form, Input } from 'antd'
-import React from 'react'
-import { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { postAdded } from './postSlice'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { postAdded } from './postsSlice'
 
 export const AddPostForm = () => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
+  const [userId, setUserId] = useState('')
 
   const dispatch = useDispatch()
+  const users = useSelector((state) => state.users)
+
+  const onTitleChanged = (e) => setTitle(e.target.value)
+  const onContentChanged = (e) => setContent(e.target.value)
+  const onAuthorChanged = (e) => setUserId(e.target.value)
 
   const onSavePostClicked = () => {
     if (title && content) {
-      dispatch(postAdded({
-        id: nanoid(),
-        title,
-        content
-      }))
+      dispatch(postAdded(title, content, userId))
+      setTitle('')
+      setContent('')
     }
-    setTitle('')
-    setContent('')
   }
+
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+
+  const usersOptions = users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ))
+
   return (
     <section>
-      <h2>添加新文章</h2>
-      <Form>
-        <Form.Item>
-          <Input name='文章标题' type='text' id='title' value={title} onChange={e => setTitle(e.target.value)} />
-        </Form.Item>
-        <Form.Item>
-          <Input.TextArea name='内容：' id='content' value={content} onChange={e => setContent(e.target.value)} />
-        </Form.Item>
-        <Form.Item>
-          <Button onClick={onSavePostClicked}>保存文章</Button>
-        </Form.Item>
-      </Form>
+      <h2>Add a New Post</h2>
+      <form>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          placeholder="What's on your mind?"
+          value={title}
+          onChange={onTitleChanged}
+        />
+        <label htmlFor="postAuthor">Author:</label>
+        <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
+          <option value=""></option>
+          {usersOptions}
+        </select>
+        <label htmlFor="postContent">Content:</label>
+        <textarea
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={onContentChanged}
+        />
+        <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
+          Save Post
+        </button>
+      </form>
     </section>
   )
 }
---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,87 @@
+import { createAsyncThunk, createSlice, nanoid } from '@reduxjs/toolkit'
+import { client } from '../../api/client'
+import { sub } from 'date-fns'
+const initialState = {
+  posts: [],
+  status: 'idle',
+  error: null
+}
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  const response = await client.get('/fakeApi/posts')
+  return response.data
+})
+
+export const addNewPost = createAsyncThunk(
+  'posts/addNewPost',
+  // payload 创建者接收部分“{title, content, user}”对象
+  async initialPost => {
+    // 我们发送初始数据到 API server
+    const response = await client.post('/fakeApi/posts', initialPost)
+    // 响应包括完整的帖子对象，包括唯一 ID
+    return response.data
+  }
+)
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    // postAdded: {
+    //   reducer (state, action) { state.posts.push(action.payload) },
+    //   prepare (title, content, userId) {
+    //     return {
+    //       payload:
+    //       {
+    //         id: nanoid(),
+    //         date: new Date().toISOString(),
+    //         title,
+    //         content,
+    //         user: userId
+    //       }
+    //     }
+    //   }
+    // },
+    postUpdated (state, action) {
+      const { id, title, content } = action.payload
+      const existingPost = state.posts.find(post => post.id === id)
+      if (existingPost) {
+        existingPost.title = title
+        existingPost.content = content
+      }
+    },
+    reactionAdded (state, action) {
+      const { postId, reaction } = action.payload
+      const existingPost = state.posts.find(post => post.id === postId)
+      if (existingPost) {
+        existingPost.reactions[reaction]++
+      }
+    }
+  },
+  extraReducers (builder) {
+    builder
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = 'loading'
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        // Add any fetched posts to the array
+        state.posts = state.posts.concat(action.payload)
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = 'failed'
+        state.error = action.error.message
+      })
+    builder.addCase(addNewPost.fulfilled, (state, action) => {
+      // 我们可以直接将新的帖子对象添加到我们的帖子数组中
+      state.posts.push(action.payload)
+    })
+  }
+})
+
+export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
+
+
+export default postsSlice.reducer
+
+export const selectAllPosts = state => state.posts.posts
+export const selectPostById = (state, postId) => state.posts.posts.find(post => post.id === postId)
